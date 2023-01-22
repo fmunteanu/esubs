70c04d02bba0f6d09f9f7a1504e9f1294fca13b3
Floren Munteanu <floren@axivo.com>
2023-01-21 20:50:39 -0500

    Initial commit

---
+++ theme/static/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    'use strict';
+    var typed = $('.typed');
+
+    $(function() {
+        typed.typed({
+            strings: [
+                'Email Is the Word'
+            ],
+            typeSpeed: 100,
+            loop: true,
+        });
+    });
+});
